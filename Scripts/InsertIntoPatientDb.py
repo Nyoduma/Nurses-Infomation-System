4df818d7014eaaf30d56dbc9307f273425d0f4d8
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-07-28 12:18:09 +0100

    transfered code from hospitalDB.py

---
+++ Scripts/InsertIntoPatientDb.py
@@ -0,0 +1,10 @@
+#INSERTING DETAILS INTO THE PATIENT TABLE
+# import sqlite3
+# conn = sqlite3.connect('unit1.db')
+# print("Opened database successfully")
+#
+# conn.execute("INSERT INTO PATIENT (PatientID,FirstName, SurName,AGE, HEIGHT,WEIGHT,BMI,BSL,ADDRESS,Mphone, EMAIL,DATEofAdmission,
+#               "TIMEOfAdmission,DoctorID,NextOfKin,NextOfKinMobile,CONDITION,CURRENTmeds,NURSINGcare,MEDICALhistory)\
+#                VALUES ('Paul','Butler' 32,175cm,90kg,29.4,4.5, '4 The Park,Ardee,County Louth',0877798711,'paul.butler@gmail.com,"
+#               "'2022/07/15','09.00hrs',5,'Mary Butler',089675645,'Peptic Ulcers',Omeprazole 400mg OD, Amoxicillin 500mg TDS,"
+#              "'Morning meds administered and patient adviced on good diet to manage symptoms', 'previous duodenal ulcer')");
