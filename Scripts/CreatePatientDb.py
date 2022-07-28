fe8939d46c508eb3b65b44d3de1b102f432d8cb3
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-07-28 12:18:01 +0100

    transfered code from hospitalDB.py

---
+++ Scripts/CreatePatientDb.py
@@ -0,0 +1,23 @@
+# conn.execute('''CREATE TABLE PATIENT
+#             (PatientID INTEGER PRIMARY KEY AUTOINCREMENT,
+#             FirstName   TEXT    NOT NULL,
+#             SurName   TEXT   NOT NULL,
+#             AGE        INT     NOT NULL,
+#             HEIGHT CHAR(10) NOT NULL,
+#             WEIGHT CHAR(10) NOT  NULL,
+#             BMI REAL NOT NULL,
+#             BSL REAL NOT NULL,
+#             ADDRESS    CHAR(50) NOT NULL,
+#             Mphone INT NOT NULL,
+#             EMAIL TEXT UNIQUE NOT NULL,
+#             DATEofAdmission TEXT NOT NULL,
+#             TIMEofAdmission TEXT NOT NULL,
+#             DoctorID INT NOT NULL,
+#             NextOfKin CHAR (20) NOT NULL,
+#             NextOfKinMobile INT NOT NULL,
+#             CONDITION CHAR(100) NOT NULL,
+#             CURRENTmeds CHAR(100) NOT NULL,
+#             NURSINGcare CHAR(500) NOT NULL,
+#             MEDICALhistory CHAR(500));''')
+# print("Table created successfully")
+# conn.close()
