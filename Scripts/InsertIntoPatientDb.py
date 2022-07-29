c6570283fded3cc13b5f8047d9c9218ee94e34ae
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-07-29 05:15:45 +0100

    Tried an insert statement but getting an error message

--- Scripts/InsertIntoPatientDb.py
+++ Scripts/InsertIntoPatientDb.py
@@ -5,7 +5,7 @@ print("Opened database successfully")
 
 conn.execute("INSERT INTO PATIENT (PatientID,FirstName, SurName,AGE, HEIGHT,WEIGHT,BMI,BSL,ADDRESS,Mphone, EMAIL,DATEofAdmission,"
               "TIMEOfAdmission,NextOfKin,NextOfKinMobile,CONDITION,CURRENTmeds,NURSINGcare,MEDICALhistory,Doctor_ID, Staff_ID)\
-              VALUES (100, 'Paul', 'Butler', 32, '175cm', '90kg', 29.4, 4.5, '4 The Park,Ardee,County Louth', 0877798711,'paul.butler@gmail.com',"
+              VALUES (100, 'James', 'Butler', 32, '175cm', '90kg', 29.4, 4.5, '4 The Park,Ardee,County Louth', 0877798711,'James.butler@gmail.com',"
               "'2022/07/15','09.00hrs', 'Mary Butler', 089675645,'Peptic Ulcers','Omeprazole 400mg OD, Amoxicillin 500mg TDS',"
               "'Morning meds administered and patient adviced on good diet to manage symptoms', 'previous duodenal ulcer',8,4)");
 conn.commit()
