821e61186f7bdef3088a26169379375c70ab215a
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-07-29 03:38:58 +0100

    When i try to insert into patient,i get an error message saying table Patient doesn not exist

--- Scripts/InsertIntoPatientDb.py
+++ Scripts/InsertIntoPatientDb.py
@@ -1,10 +1,12 @@
 #INSERTING DETAILS INTO THE PATIENT TABLE
-# import sqlite3
-# conn = sqlite3.connect('unit1.db')
-# print("Opened database successfully")
-#
-# conn.execute("INSERT INTO PATIENT (PatientID,FirstName, SurName,AGE, HEIGHT,WEIGHT,BMI,BSL,ADDRESS,Mphone, EMAIL,DATEofAdmission,
-#               "TIMEOfAdmission,DoctorID,NextOfKin,NextOfKinMobile,CONDITION,CURRENTmeds,NURSINGcare,MEDICALhistory)\
-#                VALUES ('Paul','Butler' 32,175cm,90kg,29.4,4.5, '4 The Park,Ardee,County Louth',0877798711,'paul.butler@gmail.com,"
-#               "'2022/07/15','09.00hrs',5,'Mary Butler',089675645,'Peptic Ulcers',Omeprazole 400mg OD, Amoxicillin 500mg TDS,"
-#              "'Morning meds administered and patient adviced on good diet to manage symptoms', 'previous duodenal ulcer')");
+import sqlite3
+conn = sqlite3.connect('unit1.db')
+print("Opened database successfully")
+
+conn.execute("INSERT INTO PATIENT (PatientID,FirstName, SurName,AGE, HEIGHT,WEIGHT,BMI,BSL,ADDRESS,Mphone, EMAIL,DATEofAdmission,"
+              "TIMEOfAdmission,NextOfKin,NextOfKinMobile,CONDITION,CURRENTmeds,NURSINGcare,MEDICALhistory,Doctor_ID, Staff_ID)\
+              VALUES (100, 'Paul', 'Butler', 32, '175cm', '90kg', 29.4, 4.5, '4 The Park,Ardee,County Louth', 0877798711,'paul.butler@gmail.com',"
+              "'2022/07/15','09.00hrs', 'Mary Butler', 089675645,'Peptic Ulcers','Omeprazole 400mg OD, Amoxicillin 500mg TDS',"
+              "'Morning meds administered and patient adviced on good diet to manage symptoms', 'previous duodenal ulcer',8,4)");
+conn.commit()
+con.close()
