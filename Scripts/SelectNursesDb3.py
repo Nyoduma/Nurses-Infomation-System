0705682a47d6af54acd38bdbd4904243e06027a3
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-20 08:24:30 +0100

    changes code to reflect small letters

--- Scripts/SelectNursesDb3.py
+++ Scripts/SelectNursesDb3.py
@@ -2,20 +2,20 @@
 import sqlite3 # imports sqlite3
 conn = sqlite3.connect('unit3.db') # connects and opens db
 print("Opened database successfully")
-cursor = conn.execute("SELECT  StaffID,FirstName,LastName,Password,Level,Mobile,Department,StartDate,Email,Address from NURSES")
+cursor = conn.execute("SELECT  staffid, first_name, last_name,password,level,mobile,department,start_date,email,address from nurses")
 print(cursor)
 
 for row in cursor:
-    print("StaffID = ", row[0])
-    print("FirstName = ", row[1])
-    print("LastName = ", row[2])
-    print("Password =", row[3])
-    print(" Level = ", row[4])
-    print("Mobile = ", row[5])
-    print("Department = ", row[6])
-    print("StartDate = ", row[7])
-    print("Email = ", row[8])
-    print("Address = ", row[9], "\n")
+    print("staffid = ", row[0])
+    print("first_name = ", row[1])
+    print("last_name = ", row[2])
+    print("password =", row[3])
+    print(" level = ", row[4])
+    print("mobile = ", row[5])
+    print("department = ", row[6])
+    print("startDate = ", row[7])
+    print("email = ", row[8])
+    print("address = ", row[9], "\n")
 
     print("Operation done successfully")
 
