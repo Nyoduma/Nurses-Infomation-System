8da2034e57bcfa0f54f486cd052a96071957ecc2
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-20 08:22:19 +0100

    wore code for the loging view function

--- Scripts/flaskOperation.py
+++ Scripts/flaskOperation.py
@@ -1,15 +1,47 @@
-from flask import Flask,render_template,request,redirect,url_for
+
+import sqlite3
+from flask import *
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def loging():
+    if request.method == "POST":
+        #Connect to db
+        con = sqlite3.connect("unit3.db")
+        cur = con.cursor()
+        #HTML Form
+        email = request.form['email']
+        password = request.form['password']
+        #Querry
+        cur.execute("SELECT * FROM nurses WHERE  email='"+email+"' AND password= '"+password+"'")
+        info = cur.fetchall()
+        if len(info) == 0:
+            print("Incorrect credentials entered.Please try again")
+
+        else:
+            return redirect(url_for('saveDetails'))
     return render_template('login.html')
 
 
 
+
+
+@app.route('/duties')
+def kazi():
+    return render_template('duties.html')
+
 @app.route('/add')
 def creating():
     return render_template('add.html')
+
+@app.route('/save')
+def saveDetails():
+    return render_template("success.html")
+
+
+
+
+
 
 
 
