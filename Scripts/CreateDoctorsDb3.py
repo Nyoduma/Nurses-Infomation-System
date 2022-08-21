8f496b651f41848b032c072c721bad36057b9dd1
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-21 01:32:49 +0100

    recreated  tabtle doctors

--- Scripts/CreateDoctorsDb3.py
+++ Scripts/CreateDoctorsDb3.py
@@ -2,12 +2,12 @@
 import sqlite3   #imports sqlite3
 conn = sqlite3.connect('unit3.db') # opens database
 print("Opened database successfully")
-conn.execute('''CREATE TABLE DOCTORS
-            (DoctorID INT PRIMARY KEY NOT NULL,
-            FirstName TEXT NOT NULL,
-            LastName TEXT NOT NULL,
-            Speciality TEXT NOT NULL,
-            BleepNo TEXT NOT NULL,
-            PHONE TEXT NOT NULL);''')
+conn.execute('''CREATE TABLE doctors
+            (doctorid INT PRIMARY KEY NOT NULL,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            speciality TEXT NOT NULL,
+            bleep_no TEXT NOT NULL,
+            phone TEXT NOT NULL);''')
 print("Table created successfully")
 conn.close()
