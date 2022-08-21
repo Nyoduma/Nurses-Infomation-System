22f0530ad83dc18d507432779e45b5e25f6aab94
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-21 01:32:19 +0100

    droped table doctors and inserted code in small letters

---
+++ Scripts/DropTableDoctors.py
@@ -0,0 +1,6 @@
+#Create a TABLE
+import sqlite3 # imports sqlite3
+conn = sqlite3.connect('unit3.db') #opens db
+print("Opened database successfully")
+conn.execute(" DROP TABLE DOCTORS")
+conn.close()
