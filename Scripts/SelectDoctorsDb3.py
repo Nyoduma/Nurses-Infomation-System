2ef55a2f64cf57a9d4f54c5a90206b78fe4426ae
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-21 01:34:08 +0100

    chaged code in select from doctors file to reflect small letters

--- Scripts/SelectDoctorsDb3.py
+++ Scripts/SelectDoctorsDb3.py
@@ -2,15 +2,15 @@
 import sqlite3 #imports sqlite3
 conn = sqlite3.connect('unit3.db') #opens db
 print("Opened database successfully")
-cursor = conn.execute("SELECT  DoctorID, FirstName,LastName, Speciality,BleepNo,PHONE from DOCTORS")
+cursor = conn.execute("SELECT  doctorid, first_name,last_name, speciality,bleep_no,phone from doctors")
 print(cursor)
 for row in cursor:
-    print("DoctorID = ", row[0])
-    print("FirstName = ", row[1])
-    print("LastName =", row[2])
-    print(" Speciality = ", row[3])
-    print("BleepNo = ", row[4])
-    print("PHONE = ", row[5], "\n")
+    print("doctorid = ", row[0])
+    print("first_name = ", row[1])
+    print("last_name =", row[2])
+    print("speciality = ", row[3])
+    print("bleep_no = ", row[4])
+    print("phone = ", row[5], "\n")
     print("Operation done successfully")
 
 conn.close() #closes db
