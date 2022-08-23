c5b5be4346d08e4d981d8dc1e34755273bf60d7c
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-23 11:47:04 +0100

    created  a new table patient after dropping the old PATIENT table

--- Scripts/CreatePatientDb3.py
+++ Scripts/CreatePatientDb3.py
@@ -2,25 +2,25 @@
 import sqlite3 # imports sqlite3
 conn = sqlite3.connect('unit3.db') #opens db
 print("Opened database successfully")
-conn.execute('''CREATE TABLE PATIENT
-            (PatientID INTEGER PRIMARY KEY NOT NULL,
-            FirstName   TEXT    NOT NULL,
-            SurName   TEXT   NOT NULL,
-            AGE   INTEGER     NOT NULL,
-            HEIGHT TEXT NOT NULL,
-            WEIGHT TEXT NOT  NULL,
-            BMI REAL NOT NULL,
-            Mphone TEXT NOT NULL,
-            EMAIL TEXT UNIQUE NOT NULL,
-            DATEofAdmission TEXT NOT NULL,
-            CONDITION CHAR(100) NOT NULL,
-            CURRENTmeds CHAR(100) NOT NULL,
-            NURSINGcare CHAR(100) NOT NULL,
-            MEDICALhistory CHAR(100) NOT NULL,
-            Doctor_ID INT NOT NULL,
-            Staff_ID INT NOT NULL,
-            FOREIGN KEY(Doctor_ID) REFERENCES DOCTOR(DoctorID)  
-            FOREIGN KEY (Staff_ID) REFERENCES NURSES(StaffID));''')
+conn.execute('''CREATE TABLE patient
+            (patient_id INTEGER PRIMARY KEY NOT NULL,
+            first_name TEXT NOT NULL,
+            sir_name TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            height TEXT NOT NULL,
+            weight TEXT NOT  NULL,
+            bmi REAL NOT NULL,
+            mobile_phone TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            date_of_admission TEXT NOT NULL,
+            condition CHAR(100) NOT NULL,
+            current_meds CHAR(100) NOT NULL,
+            nursing_care CHAR(100) NOT NULL,
+            medical_history CHAR(100) NOT NULL,
+            doctor_id INT NOT NULL,
+            staff_id INT NOT NULL,
+            FOREIGN KEY(doctor_id) REFERENCES DOCTOR(doctorid)  
+            FOREIGN KEY (staff_id) REFERENCES NURSES(staffid));''')
 print("Table created successfully") #prints string to console
 conn.close() #closes db
 #reference to UNIQUE CONSTRAINT https://www.sqlitetutorial.net/sqlite-unique-constraint/
