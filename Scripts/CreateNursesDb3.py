7295e0314d92135414fd627fb938862d6366df6a
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-08-27 16:21:15 +0100

    removed not null and replace with null for testing purposes

--- Scripts/CreateNursesDb3.py
+++ Scripts/CreateNursesDb3.py
@@ -4,14 +4,14 @@ conn = sqlite3.connect('unit3.db')  # opens db
 print("Opened database successfully")
 conn.execute('''CREATE TABLE nurses
             (staffid INTEGER PRIMARY KEY AUTOINCREMENT,
-            first_name TEXT  NOT NULL,
-            last_name TEXT NOT  NULL,
-            password CHAR(6) NOT NULL,
-            level TEXT NOT  NULL,
-            mobile TEXT NOT  NULL,
-            department TEXT NOT  NULL,
-            Start_date TEXT NOT  NULL,
-            email TEXT UNIQUE NOT NULL,
+            first_name TEXT  NULL,
+            last_name TEXT  NULL,
+            password CHAR(6)  NULL,
+            level TEXT  NULL,
+            mobile TEXT  NULL,
+            department TEXT NULL,
+            Start_date TEXT  NULL,
+            email TEXT UNIQUE NULL,
             address CHAR(50)  NULL);''')
 print("Table created successfully")
 conn.close() #closes db
