8aa5f34f3da10d0dde210b0e0e7d5b0b927e8787
Nyoduma <100474672+Nyoduma@users.noreply.github.com>
2022-09-05 02:18:01 +0100

    put validation msg on loging view function

--- Scripts/crud.py
+++ Scripts/crud.py
@@ -12,6 +12,8 @@ app = Flask(__name__,template_folder='templates')
 #LOGIN
 @app.route('/', methods=["POST", "GET"])
 def loging():
+    msg=""
+
     if request.method == "POST":
         # Connect to db
         con = sqlite3.connect("unit3.db")
@@ -22,20 +24,29 @@ def loging():
         #Querry
         cur.execute("SELECT * FROM nurses WHERE  email='"+email+"' AND password= '"+password+"'")
         info = cur.fetchall()
-        if len(info) == 0:
-            print("Incorrect credentials entered.Please try again")
-
-        else:
+        if info:
             return redirect(url_for('kazi'))
-    return render_template('login.html')
+        else:
+
+
+            msg = "Invalid username or password.Please try again"
+
+
+
+    return render_template('login.html', msg=msg)
+
+
 
 
 #REGISTER
 @app.route('/register', methods=["POST", "GET"])
 def register():
     msg = ""
+    con = sqlite3.connect('unit3.db')
+    cur = con.cursor
 
     if request.method == "POST":
+
         try:
             first_name = request.form['first_name']
             last_name = request.form['last_name']
